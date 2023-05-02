0f2eda636e3240f17473130728113138a597a688
HYE <jelly12paw@gmail.com>
2023-05-02 17:44:41 +0900

    chore: google login

--- src/pages/Accounts/GoogleCallback.jsx
+++ src/pages/Accounts/GoogleCallback.jsx
@@ -1,20 +1,21 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-export default function KakaoCallback() {
-
+export default function GoogleCallback() {
+    
     let backURL = process.env.REACT_APP_BACK_BASE_URL;
     const code = new URL(window.location.href).searchParams.get('code');
     useEffect(() => {
         axios({
             method: 'get',
-            url: `${backURL}accounts/kakao/login/callback/?code=${code}`,
+            url: `${backURL}accounts/google/login/callback/?code=${code}`,
         })
     }, [])
 
     return (
         <div>
-
+            
         </div>
     );
 }
+
--- src/pages/Accounts/Login.jsx
+++ src/pages/Accounts/Login.jsx
@@ -20,89 +20,94 @@ export default function Login() {
       .string()
       .required('비밀번호는 필수 입력입니다.')
       .min(8, '8자 이상 입력해주세요.'),
-    });
-    const {
-      register,
-      handleSubmit,
-      getValues,
-      formState: { errors },
-    } = useForm({
-      mode: 'onChange',
-      resolver: yupResolver(formSchema),
-    });
-    // const onSubmit = (data) => console.log(data);
-    
-    const navigate = useNavigate()
-    const dispatch = useDispatch();
+  });
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm({
+    mode: 'onChange',
+    resolver: yupResolver(formSchema),
+  });
+  // const onSubmit = (data) => console.log(data);
 
-    let backURL = process.env.REACT_APP_BACK_BASE_URL;
-    const loginRes = async (data) => {
-      const res = await axios({
-                      method: "post",
-                      url: `${backURL}accounts/login/`,
-                      data: data,
-                      // withCredentials: true,
-                  });
-              console.log(res)
-      
-              if (res.data) {
-                dispatch(loginUser(res.data.access_token));
-                navigate('/');
-                const pros = await axios.get(`${backURL}accounts/profile/`, {
-                  headers: {
-                    'Authorization' : `Bearer ${res.data.access_token}`
-                  }
-                });
-                console.log(pros.data); // 받아온 유저 정보 출력
-              } else {
-                console.log('로그인실패');
-              }
+  const navigate = useNavigate()
+  const dispatch = useDispatch();
+
+  let backURL = process.env.REACT_APP_BACK_BASE_URL;
+  const loginRes = async (data) => {
+    const res = await axios({
+      method: "post",
+      url: `${backURL}accounts/login/`,
+      data: data,
+      // withCredentials: true,
+    });
+    console.log(res)
+
+    if (res.data) {
+      dispatch(loginUser(res.data.access_token));
+      navigate('/');
+      const pros = await axios.get(`${backURL}accounts/profile/`, {
+        headers: {
+          'Authorization': `Bearer ${res.data.access_token}`
+        }
+      });
+      console.log(pros.data); // 받아온 유저 정보 출력
+    } else {
+      console.log('로그인실패');
     }
-    
-    let baseURL = process.env.REACT_APP_FRONT_BASE_URL;
-    const KAKAO_REST_API_KEY = process.env.REACT_APP_KAKAO_KEY;
-    const KAKAO_CALLBACK_URI = baseURL + '/oauth/callback/kakao/';
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_CALLBACK_URI}&response_type=code`;
-    
-    return (
-      <div className="flex flex-col justify-center items-center p-4">
-        <div className='flex bg-fixed my-2'>
-          <h1 className='font-bold text-lg'>로그인</h1>
-          <Link to='/'><RxCross2 className='text-zinc-400 mt-1.5 ml-2'/></Link>
-        </div>
-        <form onSubmit={handleSubmit(loginRes)}
-          className='w-screen flex flex-col item-center m-4 p-4'>
-          <p className='font-bold mt-3'>이메일</p>
-          <input 
-            name="email" 
-            placeholder="email@haejwo.com" 
-            {...register('email')} 
-            className='p-2 m-1 border-solid border border-zinc-400 rounded-md'/>
-          {errors.email && <p className='text-brand ml-2'>{errors.email.message}</p>}
-          <p className='font-bold mt-3'>비밀번호</p>
-          <input
-            type="password"
-            name="password"
-            placeholder="8자리 이상 영문자, 숫자 포함"
-            {...register('password')}
-            className='p-2 m-1 border-solid border border-zinc-400 rounded-md'
-          />
-          {errors.password1 && <p className='text-brand ml-2'>{errors.password1.message}</p>}
-          <input 
-            type="submit" 
-            disabled={errors && !getValues()} 
-            value="로그인하기"
-            className='p-2 m-1 mt-7 bg-brand text-white text-semibold rounded-md'/>
-        </form>
-        <h2 className="font-bold text-lg my-3">간편로그인</h2>
-        <div className="w-screen flex flex-col items-center p-4">
-          <a href={KAKAO_AUTH_URL}><button className="w-full p-2 bg-kakao flex rounded my-2 justify-center items-center"><RiKakaoTalkFill className='text-2xl mx-1'/>카카오톡으로 로그인</button></a>
-          <button className="w-full p-2 flex rounded my-2 border boreder-zinc-400 justify-center items-center"><FcGoogle className='text-2xl mx-1'/>구글로 로그인</button>
-        </div>
-        <h2 className="mt-7 font-bold text-lg">회원이 아니신가요?</h2>
-        <Link to='/signup' className="w-screen p-4">
-          <button className="w-full p-2 flex rounded my-2 border boreder-zinc-400 justify-center items-center font-bold">이메일로 회원가입</button>
-        </Link>
-      </div> 
-    );
+  }
+ // kakao login
+  let baseURL = process.env.REACT_APP_FRONT_BASE_URL;
+  const KAKAO_REST_API_KEY = process.env.REACT_APP_KAKAO_KEY;
+  const KAKAO_CALLBACK_URI = baseURL + 'oauth/callback/kakao/';
+  const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_CALLBACK_URI}&response_type=code`;
+ // google login
+  const SOCIAL_AUTH_GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+  const GOOGLE_CALLBACK_URI = backURL + 'accounts/google/login/callback/'
+  const scope = "https://www.googleapis.com/auth/userinfo.email"
+  const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${SOCIAL_AUTH_GOOGLE_CLIENT_ID}&response_type=code&redirect_uri=${GOOGLE_CALLBACK_URI}&scope=${scope}`
+
+  return (
+    <div className="flex flex-col justify-center items-center p-4">
+      <div className='flex bg-fixed my-2'>
+        <h1 className='font-bold text-lg'>로그인</h1>
+        <Link to='/'><RxCross2 className='text-zinc-400 mt-1.5 ml-2' /></Link>
+      </div>
+      <form onSubmit={handleSubmit(loginRes)}
+        className='w-screen flex flex-col item-center m-4 p-4'>
+        <p className='font-bold mt-3'>이메일</p>
+        <input
+          name="email"
+          placeholder="email@haejwo.com"
+          {...register('email')}
+          className='p-2 m-1 border-solid border border-zinc-400 rounded-md' />
+        {errors.email && <p className='text-brand ml-2'>{errors.email.message}</p>}
+        <p className='font-bold mt-3'>비밀번호</p>
+        <input
+          type="password"
+          name="password"
+          placeholder="8자리 이상 영문자, 숫자 포함"
+          {...register('password')}
+          className='p-2 m-1 border-solid border border-zinc-400 rounded-md'
+        />
+        {errors.password1 && <p className='text-brand ml-2'>{errors.password1.message}</p>}
+        <input
+          type="submit"
+          disabled={errors && !getValues()}
+          value="로그인하기"
+          className='p-2 m-1 mt-7 bg-brand text-white text-semibold rounded-md' />
+      </form>
+      <h2 className="font-bold text-lg my-3">간편로그인</h2>
+      <div className="w-screen flex flex-col items-center p-4">
+        <Link to={KAKAO_AUTH_URL} className="w-full p-2 bg-kakao flex rounded my-2 justify-center items-center"><RiKakaoTalkFill className='text-2xl mx-1' />카카오톡으로 로그인</Link>
+        <Link to={GOOGLE_AUTH_URL} className="w-full p-2 flex rounded my-2 border boreder-zinc-400 justify-center items-center"><FcGoogle className='text-2xl mx-1' />구글로 로그인</Link>
+      </div>
+      <h2 className="mt-7 font-bold text-lg">회원이 아니신가요?</h2>
+      <Link to='/signup' className="w-screen p-4">
+        <button className="w-full p-2 flex rounded my-2 border boreder-zinc-400 justify-center items-center font-bold">이메일로 회원가입</button>
+      </Link>
+    </div>
+  );
 }
