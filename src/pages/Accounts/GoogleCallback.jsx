170fff538b2310b33ba500d31d21953506bee3d3
tnpfldyd <dkdldhs0322@gmail.com>
2023-05-02 18:24:09 +0900

    fix : google login

--- src/pages/Accounts/GoogleCallback.jsx
+++ src/pages/Accounts/GoogleCallback.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 export default function GoogleCallback() {
-    
+
     let backURL = process.env.REACT_APP_BACK_BASE_URL;
     const code = new URL(window.location.href).searchParams.get('code');
     useEffect(() => {
@@ -10,11 +10,12 @@ export default function GoogleCallback() {
             method: 'get',
             url: `${backURL}accounts/google/login/callback/?code=${code}`,
         })
+            .then((res) => console.log(res))
     }, [])
 
     return (
         <div>
-            
+
         </div>
     );
 }
