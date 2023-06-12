cc8f91139fbce122d43fab8b2a88408988844d03
HYE <jelly12paw@gmail.com>
2023-06-12 22:11:35 +0900

    chore: Add Profile image uploader in Edit CO

--- src/api/EditCOInfo.jsx
+++ src/api/EditCOInfo.jsx
@@ -3,8 +3,9 @@ import axios from 'axios';
 import { useCookies } from 'react-cookie';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { saveUsername, saveBankName, saveAccountNumber, saveCategory } from '../app/useractions';
+import { saveUsername, saveBankName, saveAccountNumber, saveCategory, saveImage } from '../app/useractions';
 import COInfoForm from '../components/COInfoForm/COInfoForm';
+import ImagesUpload from '../components/ImagesUpload/ImagesUpload';
 
 export default function COUserInfo() {
     const [cookies, setCookie] = useCookies(['token']);
@@ -16,27 +17,36 @@ export default function COUserInfo() {
     const [bankName, setBankName] = useState('');
     const [accountNumber, setAccountNumber] = useState('');
     const [category, setCategory] = useState('MOVE');
+    const [file, setFile] = useState(null);
     const handleName = (name) => setUsername(name);
     const handleBank = (bank) => setBankName(bank);
     const handleAccount = (account) => setAccountNumber(account);
     const handleCategory = (category) => setCategory(category);
+    const handleFiles = (imgFiles) => setFile(imgFiles[0]);
 
-    const CORes = async (username, bankName, accountNumber, category) => {
+    const CORes = async () => {
         const token = cookies.token;
+        const formData = new FormData();
+        formData.append("username", username);
+        formData.append("bankName", bankName);
+        formData.append("accountNumber", accountNumber);
+        formData.append("category", category);
+        formData.append('image', file);
+
         try {
             const res = await axios.put(`${backURL}accounts/profile/detail/`, 
-            {
-                "username": username, "bankName": bankName, "accountNumber": accountNumber, "category": category
-            },
+            formData,
             {
               headers: {
-                'Authorization': `Bearer ${token}`
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'multipart/form-data'
               } 
             });
             dispatch(saveUsername(username));
             dispatch(saveBankName(bankName));
             dispatch(saveAccountNumber(accountNumber));
             dispatch(saveCategory(category));
+            dispatch(saveImage(URL.createObjectURL(file)));
             
             navigate('/profile');
         } catch (error) {
@@ -46,6 +56,10 @@ export default function COUserInfo() {
     return (
         <div className='w-full flex justify-center flex-col'>
             <COInfoForm name={handleName} bank={handleBank} account={handleAccount} COcategory={handleCategory}/>
+            <div className='p-4'>
+                <p className='text-lg font-bold mb-2'>프로필 이미지 업로드</p>
+                <ImagesUpload imageFiles={handleFiles}/>
+            </div>
             <button onClick={() => CORes(username, bankName, accountNumber, category)}
                 className={username === '' || bankName === '' || accountNumber === '' ? 
                     'my-4 w-full p-4 py-2 font-semibold border border-zinc-200 text-zinc-500' : 
